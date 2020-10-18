931b532c2745d6a6a5502fc0f4adfe0bd04de654
Ricardo Joel Gonzalez Velasco <70970994+ricardgo403@users.noreply.github.com>
2020-10-18 12:36:34 -0500

    feat: Bubble sort function is now implemented.
    It receives a slice of int64 and sort it in ascendant order.

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,17 @@
 package main
 
-func Burbuja(s []int64)  {
-
+func Burbuja(mySlice []int64) {
+	for i := 0; i < len(mySlice); i++ {
+		for j := i + 1; j < len(mySlice); j++ {
+			if mySlice[j] < mySlice[i] {
+				temp := mySlice[i]
+				mySlice[i] = mySlice[j]
+				mySlice[j] = temp
+			}
+		}
+	}
 }
 
-func main()  {
-	
+func main() {
+
 }
